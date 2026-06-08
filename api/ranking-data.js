db315b9e67066bc14334265bfcc87ddecc1969dc
GitHub Action <action@github.com>
2026-06-08 06:29:30 +0000

    自动更新软件榜数据 [2026-06-08]

--- api/ranking-data.js
+++ api/ranking-data.js
@@ -1,7 +1,7 @@
-// AI软件排行榜 (2026-06-01 自动更新)
+// AI软件排行榜 (2026-06-08 自动更新)
 // 数据来源: Product Hunt (https://www.producthunt.com)
 // 更新周期: 每周一自动更新
-// 自动生成时间: 2026-06-01T06:47:37.742Z
+// 自动生成时间: 2026-06-08T06:29:30.063Z
 
 export const softwareRankingData = [
   {
